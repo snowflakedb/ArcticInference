907b2d0666aba30163e815181edf69c9c90d45dc
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-06-12 23:44:55 -0700

    print

--- arctic_inference/vllm/shift_parallel.py
+++ arctic_inference/vllm/shift_parallel.py
@@ -197,6 +197,7 @@ class UlyssesParallelStatePatch(ArcticPatch[parallel_state]):
         group_ranks = all_ranks.transpose(3, 4).reshape(
             -1, shift_parallel_size).unbind(0)
         group_ranks = [x.tolist() for x in group_ranks]
+        print(f"Shift parallel group ranks: {group_ranks} ******************************")
         _SP_TP = init_model_parallel_group(group_ranks,
                                            get_world_group().local_rank,
                                            backend,
