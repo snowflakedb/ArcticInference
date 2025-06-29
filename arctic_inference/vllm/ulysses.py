1422e5ed4b43b79fec83efa68cac2ddb3f45cee3
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-06-28 23:12:14 -0700

    print

--- arctic_inference/vllm/ulysses.py
+++ arctic_inference/vllm/ulysses.py
@@ -243,8 +243,7 @@ class UlyssesParallelStatePatch(ArcticPatch[parallel_state]):
                         # print(f"{k}")
                         ranks.append(k)
                     group_ranks.append(ranks)
-        if torch.distributed.get_rank() == 0:
-            print(f"SP all-to-all group_ranks {group_ranks}")
+        print(f" ########################################## SP all-to-all group_ranks {group_ranks}")
         _SP_AA = init_model_parallel_group(group_ranks,
                                            get_world_group().local_rank,
                                            backend,
