21f41f9a7efc43c4491d75ece7abeb32231d4bcb
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-06-29 00:03:02 -0700

    test manually

--- arctic_inference/vllm/ulysses.py
+++ arctic_inference/vllm/ulysses.py
@@ -257,7 +257,7 @@ class UlyssesParallelStatePatch(ArcticPatch[parallel_state]):
         #                 # SP_AG {jj} k {k}")
         #                 ranks.append(k)
         #             group_ranks.append(ranks)
-        ranks = [[0, 4], [1, 5], [2, 6], [3, 7]]
+        group_ranks = [[0, 4], [1, 5], [2, 6], [3, 7]]
         print(f" $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ SP all-gather group_ranks {group_ranks}")
         _SP_AG = init_model_parallel_group(group_ranks,
                                            get_world_group().local_rank,
