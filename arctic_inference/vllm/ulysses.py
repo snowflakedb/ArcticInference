a7dc25eeb165880bfad005484056bead3b57e301
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-06-29 18:06:41 -0700

    test

--- arctic_inference/vllm/ulysses.py
+++ arctic_inference/vllm/ulysses.py
@@ -503,9 +503,8 @@ class UlyssesAttentionPatch(ArcticPatch[Attention]):
             # TODO: Reorder the kv__ tensor to match the original SP order
             kv_chunk = kv__.chunk(self.sp_size)
             order = torch.arange(self.sp_size)
-            # order = [0, 1, 4, 5, 2, 3, 6, 7]
-            order = [0, 2, 4, 6, 1, 3, 5, 7]
-            # order = [0, 2, 1, 3]
+            # order = [0, 2, 4, 6, 1, 3, 5, 7]
+            order = [0, 2, 1, 3]
             kv_ordered = tuple(kv_chunk[i] for i in order)
             kv__ = torch.cat(kv_ordered).contiguous()
             # unpack (key, value)
