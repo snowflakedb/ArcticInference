f2f32e33aeb5688ae043fca96c6eb597e871b71a
yewang <ye.wang@snowflake.com>
2025-09-29 21:25:53 +0000

    update builder

--- arctic_inference/op_builder/swiftkv_ops_builder.py
+++ arctic_inference/op_builder/swiftkv_ops_builder.py
@@ -8,11 +8,22 @@ class SwiftKVOpsBuilder(CUDAOpBuilder):
     def absolute_name(self):
         return f'arctic_inference.swiftkv_ops.{self.name}'
 
+    def get_prefix(self):
+        # borrowed from moe_op. refactor later
+        ai_path = self._src_path("arctic_inference")
+        return "arctic_inference" if os.path.isdir(ai_path) else ".."
+
     def sources(self):
         return [
-            '../csrc/custom_ops/torch_bindings.cpp',
-            '../csrc/custom_ops/kernels.cu',
+            'csrc/custom_ops/torch_bindings.cpp',
+            'csrc/custom_ops/kernels.cu',
         ]
+        prefix = self.get_prefix()
+        sources = [os.path.join(prefix, src) for src in sources]
+        return sources
 
     def include_paths(self):
-        return ['../csrc/custom_ops']
+        return ['csrc/custom_ops']
+        prefix = self.get_prefix()
+        sources = [os.path.join(prefix, src) for src in sources]
+        return sources
