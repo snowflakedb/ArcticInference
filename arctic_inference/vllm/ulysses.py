d4ea764343803686a1a8134cce44ffdf1828c8fa
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-10-01 02:21:39 -0700

    test

--- arctic_inference/vllm/ulysses.py
+++ arctic_inference/vllm/ulysses.py
@@ -592,7 +592,6 @@ class UlyssesDeepseekV2MLAAttention(ArcticPatch[DeepseekV2MLAAttention]):
                                    dim=-1)
         kv_c_normed = self.kv_a_layernorm(kv_c)
 
-        sp_size = parallel_state._SP.world_size
         q = q.view(-1, self.num_local_heads // sp_size, self.qk_head_dim)
         # Add head dim of 1 to k_pe
         k_pe = k_pe.unsqueeze(1)
