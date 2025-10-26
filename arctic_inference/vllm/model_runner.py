fc81f380f65cd0645e4f053681f500bdc887ec33
yewang <ye.wang@snowflake.com>
2025-10-26 23:47:27 +0000

    attempt to fix shift parallelism

--- arctic_inference/vllm/model_runner.py
+++ arctic_inference/vllm/model_runner.py
@@ -732,11 +732,18 @@ class GPUModelRunnerPatch(ArcticPatch[GPUModelRunner]):
 
         sp_size = parallel_state._SP.world_size
         tp_size = parallel_state._TP.world_size
-        compilation_cases_base = [
-            shape * sp_size for shape in compilation_cases
-            if shape * sp_size > self.shift_parallel_threshold
-            and shape * sp_size <= self.max_num_tokens
-        ]
+
+        if cudagraph_runtime_mode == CUDAGraphMode.PIECEWISE:
+            compilation_cases_base = [
+                min(shape * sp_size, self.max_num_tokens)
+                for shape in compilation_cases
+                if (shape * sp_size) > self.shift_parallel_threshold
+            ]
+        else:
+            compilation_cases_base = [
+                shape for shape in compilation_cases
+                if shape > self.shift_parallel_threshold
+            ]
 
         if is_global_first_rank():
             logger.info(f"base model (SP={sp_size}, TP={tp_size}) shapes {compilation_cases_base}")
