daa445254eb791f4443d403f6dbf6435f04f445b
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-12-05 12:19:11 -0800

    Clean up apply_vllm_bench_patch_v10p1.sh

--- benchmark/trace/apply_vllm_bench_patch_v10p1.sh
+++ benchmark/trace/apply_vllm_bench_patch_v10p1.sh
@@ -76,54 +76,6 @@ patch "$SERVE_PY" << 'EOF'
 267,270d250
 <     print(f"requsest, timestamp, input_len, output_len, ttft_ms, tpot_ms, e2el_ms")
 <     for i in range(len(outputs)):
-<         print(f"{i} {input_requests[i].trace_timestamp} {input_requests[i].prompt_len} {actual_output_lens[i]} {ttfts[i] * 1000:.2f} {all_tpots[i] * 1000:.2f} {e2els[i] * 1000:.2f}")
-< 
-356d335
-<     trace_dataset_path: str | None = None,
-477,488d455
-<     if trace_dataset_path is not None:
-<         iterator = get_request_trace(input_requests)
-<     else:
-<         iterator = get_request(
-<             input_requests,
-<             request_rate,
-<             burstiness,
-<             ramp_up_strategy,
-<             ramp_up_start_rps,
-<             ramp_up_end_rps,
-<         )
-< 
-501c468,470
-<     async for request, current_request_rate in iterator:
----
->     async for request, current_request_rate in get_request(
->             input_requests, request_rate, burstiness, ramp_up_strategy,
->             ramp_up_start_rps, ramp_up_end_rps):
-987d955
-<     parser.add_argument("--trace-dataset-path", type=str, default=None)
-1096d1063
-<             trace_dataset_path=args.trace_dataset_path,
-(myvenv) yak@research-exemption-job-21-mpimaster-0:~/myvenv/lib/python3.10/site-packages/vllm/benchmarks$ diff serve.py serve.py.orig 
-101,116d100
-< async def get_request_trace(input_requests: list[SampleRequest]) -> AsyncGenerator[tuple[SampleRequest, float], None]:
-< 
-<     print(f"get_request_trace")
-< 
-<     prev_ts = 0
-<     for i in range(len(input_requests)):
-<         request = input_requests[i]
-<         curr_ts = request.trace_timestamp
-<         delay = curr_ts - prev_ts
-<         print(f"request {i} prompt_len {request.prompt_len} expected_output_len {request.expected_output_len} timestamp {request.trace_timestamp} delay {delay} ms")
-<         if delay > 0:
-<             await asyncio.sleep(delay/1000)
-<         prev_ts = curr_ts
-<         yield request, 1
-< 
-< 
-267,270d250
-<     print(f"requsest, timestamp, input_len, output_len, ttft_ms, tpot_ms, e2el_ms")
-<     for i in range(len(outputs)):
 <         print(f"{i} {input_requests[i].trace_timestamp} {input_requests[i].prompt_len} {actual_output_lens[i]} {ttfts[i]*1000:.2f} {all_tpots[i]*1000:.2f} {e2els[i]*1000:.2f}")
 < 
 356d335
