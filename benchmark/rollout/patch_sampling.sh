ee22db0e18c09071eacc8f7af18d2c9a8f875416
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2026-01-31 14:50:34 -0800

    Clean up commented paths in patch_sampling.sh

--- benchmark/rollout/patch_sampling.sh
+++ benchmark/rollout/patch_sampling.sh
@@ -1,8 +1,6 @@
 
 
 VLLM_PATH="/home/yak/myenv_v14/lib/python3.12/site-packages/vllm"
-# SAMPLING_PARAMS_PATH="/home/yak/myenv_v14/lib/python3.12/site-packages/vllm/sampling_params.py"
-# PARALLEL_SAMPLING_PATH="/home/yak/myenv_v14/lib/python3.12/site-packages/vllm/v1/engine/parallel_sampling.py"
 
 patch $VLLM_PATH/sampling_params.py < sampling_params.patch
 patch $VLLM_PATH/v1/engine/parallel_sampling.py < parallel_sampling.patch
